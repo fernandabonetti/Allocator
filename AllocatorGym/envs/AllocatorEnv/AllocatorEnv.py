Fixing peak to match usage units

AllocatorGym/envs/AllocatorEnv/AllocatorEnv.py
@@ -22,13 +22,13 @@ class AllocatorEnv(gym.Env):
 		self.node_max_cpu = self.collector.getNodeCPU()
 
 		# Observation Space is a box with 3-tuple elements
-		self.observation_space = spaces.Box(low=0, high=10000, shape=(2,3), dtype=np.float32)
+		self.observation_space = spaces.Box(low=0, high=10000, shape=(1,6), dtype=np.float32)
 		self.action_space = spaces.Discrete(len(ACTIONS))
 		
 
 	def _take_action(self, action):
 		cpu_thresh, mem_thresh = ACTIONS[action]
-		
+		print(cpu_thresh, mem_thresh)
 		cpu_resize = 0
 		mem_resize = 0
 		if cpu_thresh > 0:
@@ -41,7 +41,7 @@ class AllocatorEnv(gym.Env):
 		self.mem_limit += mem_resize
 		self.mem_request += mem_resize
 		#print("[parameters]", self.cpu_limit, self.mem_limit, self.cpu_request, self.mem_request)
-		command = 'kubectl set resources deployment ' + self.container + ' --limits=cpu=' + str(self.cpu_limit) +'m,memory=' + str(self.mem_limit) + 'Mi --requests=cpu=' + str(self.cpu_request) + 'm,memory=' + str(self.mem_request) + 'Mi'
+		command = 'kubectl set resources deployment ' + self.container + ' --limits=cpu=' + str(math.floor(self.cpu_limit)) +'m,memory=' + str(math.floor(self.mem_limit)) + 'Mi --requests=cpu=' + str(math.floor(self.cpu_request)) + 'm,memory=' + str(math.floor(self.mem_request)) + 'Mi'
 		print(command)
 		subprocess.run(command, shell=True)
 
@@ -50,17 +50,16 @@ class AllocatorEnv(gym.Env):
 		self._take_action(action)
 		
 		cpu_usage, mem_usage = self.collector.getResourceUsage()
-		next_state = ((cpu_usage, self.cpu_request, self.cpu_limit), (mem_usage, self.mem_request, self.mem_limit))
+		next_state = (cpu_usage, self.cpu_request, self.cpu_limit, mem_usage, self.mem_request, self.mem_limit)
 			
 		if cpu_usage > self.cpu_limit or mem_usage > self.mem_limit:
-			print("vo mata esse container")
 			done = True
 		if self.cpu_limit >= self.node_max_cpu or self.mem_limit >= self.node_max_memory:
 			done = True	
 
-		peak_mem = ((self.mem_limit - self.mem_request) * peak)/100
-		peak_cpu = ((self.cpu_limit - self.cpu_request) * peak)/100
-
+		peak_mem = self.mem_request + (((self.mem_limit - self.mem_request) * peak)/100) # transform peak to be limits relative
+		peak_cpu = self.cpu_request + (((self.cpu_limit - self.cpu_request) * peak)/100)
+		print("[peak]", peak_cpu, peak_mem)
 		reward =  (a * (1 - abs(cpu_usage - peak_cpu)/100)) + (b * (1 - abs(mem_usage - peak_mem)/100))
 		return np.array(next_state), reward, done    
 		
@@ -72,7 +71,7 @@ class AllocatorEnv(gym.Env):
 		self.cpu_request, self.mem_request = self.collector.getResourceRequests()
 		self.cpu_limit, self.mem_limit = self.collector.getResourceLimits()
 
-		return np.array(((cpu_usage, self.cpu_request, self.cpu_limit), (mem_usage, self.mem_request, self.mem_limit)))
+		return np.array((cpu_usage, self.cpu_request, self.cpu_limit, mem_usage, self.mem_request, self.mem_limit))
 
 	def render(self):
 		pass

DQNAgent.py
@@ -20,39 +20,39 @@ class DQNAgent():
 		self.epsilon_min = 0.01
 
 		self.model = self._build_model()
-		self.target_model = self._build_model()
+		self.model.summary()
+		#self.target_model = self._build_model()
 
 	def _build_model(self):
 		model = Sequential()
-		model.add(Dense(6, input_dim=3, activation='relu'))
+		model.add(Dense(6, input_shape=(6,), activation='relu'))
 		model.add(Dense(49, activation='relu')) 
 		model.add(Dense(self.action_size, activation='linear')) # 49 actions on output layer
 		model.compile(loss='mse', optimizer=Adam(lr=self.alpha))
 		return model
 
 	def store_experience(self, state, action, reward, next_state, done):
-		self.replay_memory.append([state, action, reward, next_state, done])
+		self.replay_memory.append((state, action, reward, next_state, done))
 			
 	def sample_action(self, state):
 		if self.epsilon >= np.random.rand():
 			return random.randrange(self.action_size)
 		action_values = self.model.predict(state)
-		print(action_values)
 		return np.argmax(action_values[0])  
 
 	def replay(self, batch_size): 
 		minibatch = random.sample(self.replay_memory, batch_size) 
 		for state, action, reward, next_state, done in minibatch:
-			print("replay") 
-			target = self.target_model.predict(state) 
-			target[0][action] = reward 
+			target = reward
+			 
 			if not done:
-				q_future = max(self.target_model.predict(next_state)[0])
-				target[0][action] = reward + q_future * self.gamma
-			self.model.fit(state, target, epochs=1, verbose=0) 
+				target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+			q_future = self.model.predict(state)
+			q_future[0][action] = target
+			self.model.fit(state, q_future, epochs=1, verbose=0) 
 
-			if self.epsilon > self.epsilon_min:
-				self.epsilon *= self.epsilon_decay
+		if self.epsilon > self.epsilon_min:
+			self.epsilon *= self.epsilon_decay
 
 	def target_train(self):
 		weights = self.model.get_weights()
@@ -63,4 +63,4 @@ class DQNAgent():
 		self.model.load_weights(name)
 
 	def save(self, name):
-		self.model.save_weights(name)        
+		self.model.save_weights(name) 

main.py
@@ -1,5 +1,6 @@
 import gym
 import envs
+import time
 import numpy as np
 from DQNAgent import DQNAgent
 
@@ -11,7 +12,7 @@ n_episodes = 1000
 batch_size = 32
 a = 0.5
 b = 0.5
-peak = 50
+peak = 25
 
 env = gym.make('Allocator-v0', ip=ip, port=port,  container=container)
 
@@ -19,16 +20,19 @@ state_size = env.observation_space.shape[0]
 action_size = env.action_space.n
 
 agent = DQNAgent(state_size, action_size, a, b, peak)
-
+done = False
 for episode in range(n_episodes):
-	state = env.reset().reshape(2, 3)
+	state = env.reset()
+	state = np.reshape(state, [1,6])
 
-	for timestep in range(500):
+	for timestep in range(5000):
+		#time.sleep(5)
+		
 		action = agent.sample_action(state)
 
 		next_state, reward, done = env.step(action, a, b, peak)
 
-		next_state.reshape(2, 3)
+		next_state = np.reshape(next_state, [1, 6])
 
 		reward = reward if not done else -1  # punish agent if it fails
 		print("[REWARD]:", reward)
@@ -40,8 +44,8 @@ for episode in range(n_episodes):
 			break
 
 		# change to 50	
-		if timestep % 100 == 0:
-			agent.target_train()
+		#if timestep % 100 == 0:
+		#	agent.target_train()
 
 	if len(agent.replay_memory) > batch_size:
 		agent.replay(batch_size)