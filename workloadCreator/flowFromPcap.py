6c88dac62adada8aaf610aa408b723ec8bcf673e
fernandabonetti <fernandasbonetti@gmail.com>
2021-01-15 22:57:20 -0300

    Change workload to CAIDA

--- workloadCreator/flowFromPcap.py
+++ workloadCreator/flowFromPcap.py
@@ -11,23 +11,20 @@ def main():
 		print("Provide the filename of a .pcap archive")
 		exit(1)
 	filename = sys.argv[1]
-	packets = rdpcap(filename)
+	packets = rdpcap(filename, 85000)
 
-	payload = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 500))
-	template = IP(dst="192.168.49.2")/UDP(sport=5080, dport=52148)/Raw(load=payload)
+	timestamp = [str(packets[i+1].time-packets[i].time)+'\n' for i in range(1, len(packets)-1)]
+	# length = [str(packet.len)+'\n' for packet in packets]
+	length = []
+	for packet in packets:
+		try:
+			length.append(str(packet.len)+'\n')
+		except AttributeError:
+			length.append('500\n')
 
-	#timestamp = []
-	craftedPackets = []
-	for i in range(0, len(packets)-1):
-		# extract timestamp from packet
-		#if i > 0: timestamp.append(packets[i+1].time-packets[i].time)
-		craftedPackets.append(template)
-		template.show2()
-	wrpcap("trigger-workload.pcap", craftedPackets)
-	
-	with open('timestamps.txt', 'w') as fp:
-		for time in timestamp:
-			fp.write(str(time)+'\n')
+	with open('timestamps.txt', 'a') as fp, open('length.txt', 'a') as fr:
+		fp.writelines(timestamp)
+		fr.writelines(length)
 
 if __name__ == '__main__':
 	main()
--- workloadCreator/sendPackets.py
+++ workloadCreator/sendPackets.py
@@ -3,12 +3,15 @@ import string
 import random 
 
 def main():
-	payload = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 500))
-	with open('timestamps.txt', 'r') as fp:
+	payload = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 1500))
+	with open('timestamps.txt', 'r') as fp, open('length.txt', 'r') as fr:
 		delay = [float(line) for line in fp.readlines()]
+		length = [int(line) for line in fr.readlines()]
 
 	for i in range(0, len(delay)):
-		send(IP(dst='192.168.49.2')/UDP(sport=5666, dport=5444)/Raw(payload), return_packets=True)
+		template = IP(dst='192.168.49.2')/UDP(sport=5666, dport=5444)
+		padding = payload[0:(length[i]-28)]			#28 is to fill the size of previous layers 
+		send(template/Raw(padding), return_packets=True)
 		time.sleep(delay[i])
 	
 if __name__ == '__main__':
