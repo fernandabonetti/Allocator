e89aa5c1fc489a93d586df6cd2d438e14f872da9
fernandabonetti <fernandasbonetti@gmail.com>
2022-06-05 17:17:11 -0300

    add port forwarding

--- services/vpa/parser.sh
+++ services/vpa/parser.sh
@@ -4,9 +4,10 @@
 kubectl apply -f deployment.yaml
 sleep 30s
 
+kubectl port-forward svc/nginx 5000:80
+
 #periodically generate vpa reports
 python3 parser.py > outputvpa.txt
-
 sleep 30
 
 #cleanup
